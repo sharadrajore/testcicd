c890af9b9921063ca3575c76fbbac2bd5327100c
sharad <sharad.rajore@gmail.com>
2021-07-27 16:22:48 +0530

    first commit

---
+++ src/main/java/com/zensar/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.zensar.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		System.out.println("Hello");
+		return "<h2> Hello Jenkins !!! </h2> ";
+	}
+
+	
+}
